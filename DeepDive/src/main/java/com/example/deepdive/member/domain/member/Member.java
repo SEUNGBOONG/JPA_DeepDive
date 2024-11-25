4426ed282e3ae82cf87a492aa7ed8f8c2e12846a
승엽 <tmdduqflfl@naver.com>
2024-11-25 18:50:21 +0900

    11/25 엔티티생성

---
+++ DeepDive/src/main/java/com/example/deepdive/member/domain/member/Member.java
@@ -0,0 +1,35 @@
+package com.example.deepdive.member.domain.member;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String memberEmail;
+
+    @Column(nullable = false)
+    private String memberName;
+
+    @Column(nullable = false)
+    private String memberPassword;
+
+    @Column(nullable = false)
+    private String memberNickName;
+
+}
+
+
